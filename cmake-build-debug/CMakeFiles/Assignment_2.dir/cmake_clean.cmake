4f9c6a8087d5ecbfa67f196ebfdbf3923b4fdc5b
cleeag <jack0506000@gmail.com>
2020-04-12 13:40:55 +0800

    initial commit

---
+++ cmake-build-debug/CMakeFiles/Assignment_2.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "Assignment_2"
+  "Assignment_2.pdb"
+  "CMakeFiles/Assignment_2.dir/Controller.cpp.o"
+  "CMakeFiles/Assignment_2.dir/Object.cpp.o"
+  "CMakeFiles/Assignment_2.dir/TodoCattlefarm.cpp.o"
+  "CMakeFiles/Assignment_2.dir/TodoCow.cpp.o"
+  "CMakeFiles/Assignment_2.dir/TodoEmployee.cpp.o"
+  "CMakeFiles/Assignment_2.dir/TodoFarmer.cpp.o"
+  "CMakeFiles/Assignment_2.dir/TodoFarmland.cpp.o"
+  "CMakeFiles/Assignment_2.dir/TodoFeeder.cpp.o"
+  "CMakeFiles/Assignment_2.dir/TodoProperty.cpp.o"
+  "CMakeFiles/Assignment_2.dir/TodoScene.cpp.o"
+  "CMakeFiles/Assignment_2.dir/View.cpp.o"
+  "CMakeFiles/Assignment_2.dir/main.cpp.o"
+  "CMakeFiles/Assignment_2.dir/test.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Assignment_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
