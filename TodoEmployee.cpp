25001247f00821595aece67da4472d114d3dc0ec
cleeag <jack0506000@gmail.com>
2020-04-12 18:30:18 +0800

    81 done, 40%

--- TodoEmployee.cpp
+++ TodoEmployee.cpp
@@ -13,14 +13,15 @@ const int EMPLOYEE_SALARY = 1; // Get paied when in both work and rest states.
 const int EMPLOYEE_NUM_WORK_DAYS = 1;
 const int EMPLOYEE_NUM_REST_DAYS = 1;
 // All types of Employees have size 1.
-const int ALLEMPLOYEE_SIZE_X = 1; const int ALLEMPLOYEE_SIZE_Y = 1;
+const int ALLEMPLOYEE_SIZE_X = 1;
+const int ALLEMPLOYEE_SIZE_Y = 1;
 
 Employee::Employee()
-: Employee(EMPLOYEE_COST, EMPLOYEE_SALARY, EMPLOYEE_NUM_WORK_DAYS, EMPLOYEE_NUM_REST_DAYS) {
+        : Employee(EMPLOYEE_COST, EMPLOYEE_SALARY, EMPLOYEE_NUM_WORK_DAYS, EMPLOYEE_NUM_REST_DAYS) {
 }
 
 Employee::Employee(int cost, int salary, int ndays_work, int ndays_rest)
-: m_cost(cost), m_salary(salary), m_ndays_work(ndays_work), m_ndays_rest(ndays_rest) {
+        : m_cost(cost), m_salary(salary), m_ndays_work(ndays_work), m_ndays_rest(ndays_rest) {
     setSize(ALLEMPLOYEE_SIZE_X, ALLEMPLOYEE_SIZE_Y);
 }
 
@@ -28,10 +29,89 @@ Employee::~Employee() {
     //Auto-generated destructor stub
 }
 
-void Employee::writeToStream(ostream& os) const {
-	Object::writeToStream(os);
+void Employee::writeToStream(ostream &os) const {
+    Object::writeToStream(os);
     os << "Work Age: " << getWorkAge() << endl;
     os << "Salary: " << getSalary() << endl;
 }
 
 // TODO: Start to implement your code.
+
+//Return the cost of the employee.
+int Employee::getCost() const {
+    return m_cost;
+}
+
+
+//Return the number of work days of the employee.
+int Employee::getNumWorkDays() const {
+    return m_ndays_work;
+}
+
+//Return the number of rest days of the employee.
+int Employee::getNumRestDays() const {
+    return m_ndays_rest;
+}
+
+//Return the work age of the employee.
+int Employee::getWorkAge() const {
+    return m_work_age;
+}
+
+//Return the salary according to the specification.
+int Employee::getSalary() const {
+    return m_salary;
+}
+
+//Update the state: Work/Rest according to the m_work_age and the specification
+//If the employee has not been assigned to a property,
+//Its state is always NORMAL.
+//If the employee has been assigned.
+//Its state changes between WORK(first)/REST.
+void Employee::updateState() {
+    int work_age = getWorkAge();
+    if (work_age == 0) {
+        setState(ObjectState::WORK);
+    } else if (getName() == "Employee") {
+        if (work_age % 2 == 1) {
+            setState(ObjectState::REST);
+        } else if (work_age % 2 == 0) {
+            setState(ObjectState::WORK);
+        }
+    } else if (getName() == "Farmer") {
+        if (work_age % 4 == 3) {
+            setState(ObjectState::REST);
+        } else if (work_age % 4 == 0) {
+            setState(ObjectState::WORK);
+        }
+    } else if (getName() == "Feeder") {
+        if (work_age % 7 == 6) {
+            setState(ObjectState::REST);
+        } else if (work_age % 7 == 0) {
+            setState(ObjectState::WORK);
+        }
+    } else if (getName() == "Cow") {
+        if (work_age % 2 == 1) {
+            setState(ObjectState::REST);
+        } else if (work_age % 2 == 0) {
+            setState(ObjectState::WORK);
+        }
+    }
+
+    updateWorkAge();
+}
+
+//Increase the workage by one
+void Employee::updateWorkAge() {
+    m_work_age += 1;
+}
+
+//return the "Employee"
+string Employee::getName() const {
+    return "Employee";
+}
+
+//return the "e" (Captain Sensitive)
+char Employee::getSymbol() const {
+    return 'e';
+}
--- TodoProperty.cpp
+++ TodoProperty.cpp
@@ -30,3 +30,85 @@ void Property::getConstEmployeeList(const Employee**& employee) const {
 }
 
 // TODO: Start to implement your code.
+
+// The first parameter is the cost to build the property.
+// The second parameter is the cost to upgrade the property.
+// The third parameter is the max number of employees.
+Property::Property(int m_cost, int m_upgrade_cost, int m_max_num_employee){
+    this->m_cost = m_cost;
+    this->m_upgrade_cost = m_upgrade_cost;
+    this->m_max_num_employee = m_max_num_employee;
+}
+
+Property::~Property(){
+    for (int i = 0; i < m_num_employee; ++i) {
+        delete m_employee_list[i];
+    }
+}
+
+// Return the building cost of the property.
+int Property::getCost() const {
+    return m_cost;
+}
+
+// Return the upgrade cost of the property.
+int Property::getUpgradeCost() const{
+    return m_upgrade_cost;
+}
+
+// Return the level of the property.
+int Property::getLevel() const{
+    return m_level;
+}
+
+// Return the number of the employees assigned to the property.
+int Property::getNumEmployee() const{
+    return m_num_employee;
+}
+
+// Return the max number of the employees.
+int Property::getMaxNumEmployee() const{
+    return m_max_num_employee;
+}
+
+// Set the max number of the employees.
+void Property::setMaxNumEmployee(int m){
+    m_max_num_employee = m;
+}
+
+
+// Assign an employee to the property.
+// You should update the m_employee_list (with shallow copying), m_num_employee
+// and set the x,y of Employee.
+// All the employees should always be inside the range of their property.
+// If the Property, e.g.Farmland(R), is at (0, 3) with size (5, 5),
+// the first employee should at (1, 4);
+// the second employee should at (1, 5);
+//  012346x
+// 3RRRRR
+// 4R147R
+// 5R258R
+// 6R369R
+// 7RRRRR
+// y
+// If reaches m_max_num_employee, it fails to assign.
+// If the employee already in the list, it fails to assign.
+// If the employee is unsuitable to the property, it fails to assign.
+// Return true, if assign successfully.
+// Otherwise, return false.
+bool Property::assignEmployee(Employee*);
+
+// Fire an employee.
+// You should update the m_employee_list, m_num_employee
+// and completely delete the employee.
+// Assumption: You can assume the input parameter is in the m_employee_list if m_employee_list is not nullptr.
+// Hint: You have to take care of the position of other employees after deleting one.
+// Otherwise, it may cause a problem when you assigning new employees.
+// Please note all the employees should always be inside their property.
+// We gave some examples to clarify the "inside" and "outside" on the webpage.
+// Return true, if assign successfully.
+bool Property::fireEmployee(Employee*);
+
+
+//return ObjectType::PROPERTY to identify that is an EMPLOYEE type.
+virtual ObjectType getObjectType() const override {return ObjectType::PROPERTY;}
